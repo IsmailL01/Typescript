5c37e2fa6912216c999cab41587a9e3d8e5d3f40
Ismail <hummetzadeismail@gmail.com>
2023-08-04 03:20:43 +0300

    typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
 const path = require("pash")
 
 module.exports = {
-    entry: ""
+    mode: "development",
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: '/\.tsx?$/',
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist")
+    }
 }
